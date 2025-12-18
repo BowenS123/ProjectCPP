22759f0df7e2bc652e814278cf5c3e52ee4531cb
BowenS123 <bowen.liang@student.pxl.be>
2025-12-18 19:51:28 +0100

    trying to fix export

--- ClankerFactory/build/Desktop_Qt_6_10_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_ClankerFactory_FILE C:/Users/bowen/OneDrive/Documenten/ClankerFactory/build/Desktop_Qt_6_10_0_MinGW_64_bit-Debug/ClankerFactory.exe)
-set(__QT_DEPLOY_TARGET_ClankerFactory_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_ClankerFactory_RUNTIME_DLLS C:/Qt/6.10.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.10.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.10.0/mingw_64/bin/Qt6Core.dll)
